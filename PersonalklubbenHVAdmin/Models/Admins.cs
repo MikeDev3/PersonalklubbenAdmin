cc7271ff51e21d85aeb4c5eba8314d7b1b657df9
MikeDev3 <M.danielsson@hotmail.se>
2019-12-02 12:58:32 +0100

    Modeller klara

---
+++ PersonalklubbenHVAdmin/Models/Admins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalklubbenHVAdmin.Models
+{
+    public class Admins
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Förnamn { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Efternamn { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Epostadress { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Lösenord { get; set; }
+    }
+}
---
+++ PersonalklubbenHVAdmin/Models/Nyheter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PersonalklubbenHVAdmin.Models
+{
+    public class Nyheter
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Rubrik { get; set; }
+
+        [StringLength(100)]
+        public string Underrubrik { get; set; }
+
+        [Column(TypeName = "text")]
+        [Required]
+        public string Beskrivning { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime PubliceringsDatum { get; set; }
+    }
+}
+}
