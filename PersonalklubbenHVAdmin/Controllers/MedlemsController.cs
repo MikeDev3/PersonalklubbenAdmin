a23506a3f003d2af718c3a1abe1545d991c4bfff
MikeDev3 <M.danielsson@hotmail.se>
2019-12-10 13:04:02 +0100

    All

--- PersonalklubbenHVAdmin/Controllers/MedlemsController.cs
+++ PersonalklubbenHVAdmin/Controllers/MedlemsController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -44,7 +46,9 @@ namespace PersonalklubbenHVAdmin.Controllers
         }
         public ActionResult NewRegistrations()
         {
-            return View();
+            List<Appliances> newMembers = new List<Appliances>();
+            newMembers = ShowRegistrations();
+            return View(newMembers);
         }
 
         public ActionResult ShowProfile(int ID)
@@ -81,6 +85,67 @@ namespace PersonalklubbenHVAdmin.Controllers
                 return View();
             }
         }
+
+        public ActionResult ApproveMembership (int id)
+        {
+            Appliances appliance = new Appliances();
+            appliance = getAppliance(id);
+
+            Medlem newMember = new Medlem();
+            newMember.Förnamn = appliance.Firstname;
+            newMember.Efternamn = appliance.Lastname;
+            newMember.Epostadress = appliance.Email;
+            newMember.GiltighetsÅr = appliance.ValidToDate;
+            newMember.RegistreringsDatum = appliance.RegistreredDate;
+            newMember.Telefonnummer = appliance.PhoneNumber;
+            newMember.Institution = appliance.Institution;
+
+            return View(newMember);
+
+        }
+        [HttpPost, ActionName("ApproveMembership")]
+        [ValidateAntiForgeryToken]
+        /*
+         Metod för att bekräfta radering av en användare, här utförs således den faktiska raderingen.
+         Metoden tar den specifika användarens Id som inparamter.
+         */
+        public async Task<ActionResult> ConfirmRegistration(Medlem nyMedlem)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://193.10.202.76/");
+
+                    var myContent = JsonConvert.SerializeObject(nyMedlem);
+                    var buffer = Encoding.UTF8.GetBytes(myContent);
+                    var byteContent = new ByteArrayContent(buffer);
+                    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                    var result = client.PostAsync("PhersonalklubbenREST/api/Medlemmars", byteContent).Result;
+                    string data = await result.Content.ReadAsStringAsync();
+                    if (result.IsSuccessStatusCode)
+                    {
+                        ModelState.AddModelError("Felmeddelande", "Medlem registrerad");
+                        return RedirectToAction("MedlemsIndex");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Felmeddelande", "Denna användare kan inte hittas.");
+                        return RedirectToAction("NewRegistrations");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError("Felmeddelande", "Denna användare kan inte hittas.");
+                return RedirectToAction("NewRegistrations");
+            }
+        }
+
         public List<Medlem> ShowMembers()
         {
             try
@@ -106,6 +171,56 @@ namespace PersonalklubbenHVAdmin.Controllers
 
             }
 
+        }
+        public List<Appliances> ShowRegistrations()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://193.10.202.76/");
+                    MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                    client.DefaultRequestHeaders.Accept.Add(contentType);
+                    HttpResponseMessage response = client.GetAsync("/PhersonalklubbenREST/api/Appliances").Result;
+                    string stringData = response.Content.ReadAsStringAsync().Result;
+                    List<Appliances> data = JsonConvert.DeserializeObject<List<Appliances>>(stringData);
+                    return data;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //ToDo Give errormessage to user and possibly log error
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+                List<Appliances> emptyList = new List<Appliances>();
+                return emptyList;
+
+            }
+        }
+        public Appliances getAppliance (int id)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://193.10.202.76/");
+                    MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                    client.DefaultRequestHeaders.Accept.Add(contentType);
+                    HttpResponseMessage response = client.GetAsync("/PhersonalklubbenREST/api/Appliances/" + id).Result;
+                    string stringData = response.Content.ReadAsStringAsync().Result;
+                    Appliances data = JsonConvert.DeserializeObject<Appliances>(stringData);
+                    return data;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //ToDo Give errormessage to user and possibly log error
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+                Appliances emptyObject = new Appliances();
+                return emptyObject;
+
+            }
         }
     }
 }
---
+++ PersonalklubbenHVAdmin/Models/Appliances.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PersonalklubbenHVAdmin.Models
+{
+    public class Appliances
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Institution { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime ValidToDate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime RegistreredDate { get; set; }
+    }
+}
